34cda57c440ee1e763c737a7c50d27792bb4b168
bheem <bhimv9889@gmail.com>
2026-03-19 15:14:33 +0530

    fix port and env variable

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,7 +1,7 @@
+import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv";
 import notesRouter from "./router/noteRouter.js";
 import authUser from "./middleware/authUser.js";
 import userRouter from "./router/userRoutes.js";
@@ -9,7 +9,7 @@ import userRouter from "./router/userRoutes.js";
 dotenv.config();
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
